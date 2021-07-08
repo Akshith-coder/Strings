785443fa4b609dda7f2362b0d4329354dc4bef10
AkshithKachaka1 <67799241+Akshith-coder@users.noreply.github.com>
2021-07-08 17:02:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#Strings
+''''
+
+Strings are words and letters in Python
+
+Using either quotation marks doesn't change the value of the string
+'hello' is the same as "hello"
+
+'''
+#str()
+''''
+the str() function is used to turn most values into String
+'''
+#len()
+''''
+used to find how many letters are in the string
+'''
+x = 10
+print(str(x))
